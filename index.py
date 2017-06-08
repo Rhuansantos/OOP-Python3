0a93978bb930f962deca6795ec6620e3d16ee50d
Rhuan Souza <rhuansantosdev@gmail.com>
2017-06-08 17:19:56 -0400

    Validation for type field

--- index.py
+++ index.py
@@ -1,30 +1,42 @@
 class Shelter:
     def __init__(self):
-        self.shelterId = 0
+        self.shelterId = '0'
         self.shelterLocation = 'Orlando-Fl'
-        return Shelter
-
 
 class Pet(Shelter):
     def __init__(self, _name, _age, _type):
+        super().__init__()
         self.name = _name
         self.age = _age
         self.type = _type
-        Pet.validation(_type)
+        self.validation(self.type)
+        self.petProfile = [_name, _age, _type, self.shelterLocation]
 
-    def validation(self):
-        #if self.age == "dog"
-        yield 'hello pet'
+    def validation(self, _type):
+        if _type == "dog" or _type == "cat":
+            print('everything looks fine')
+        else:
+            while _type is not "dog" or _type is not "cat":
+                print('======= Oops, type dog or cat =======')
+                _type = input()
+                if _type == "dog" or _type == "cat":
+                    self.printPetProfile()
+                    return True
+
+    def printPetProfile(self):
+        print()
+
+
 
 
 # Create a new pet
 # name = input("Type the name of the pet")
 # age = input("Type the age")
-# age = input("Type the type")
+# petType = input("Type the type")
+# newPet = Pet(name, age, petType)
 
-newPet = Pet('rhuan', '10', 'dog')
-print(newPet)
 
-a = Pet.validation
+newPet = Pet('rhuan', '10', 'fas')
 
-print(a)
+#
+# print(newPet.validation())
