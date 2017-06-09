c10f9ec7b42681c6e53332a11b8ccb32bcead42d
Rhuan Souza <rhuansantosdev@gmail.com>
2017-06-08 23:21:52 -0400

    Makes the script continually running

--- index.py
+++ index.py
@@ -1,65 +1,68 @@
-class utilities:
-    def __init__(self):
+# never end
+while True:
 
-        def avg(self):
-            return 'this is your avg'
+    class utilities:
+        def __init__(self):
 
-class Shelter:
-    def __init__(self):
-        self.shelterId = '0'
-        self.shelterLocation = 'Orlando-Fl'
+            def avg(self):
+                return 'this is your avg'
 
-class Pet(Shelter):
-    def __init__(self, _name, _age, _type):
-        super().__init__()
-        self.name = _name
-        self.age = _age
-        self.type = _type
-        self.validation(self.type)
-        self.petProfile = [_name, _age, _type, self.shelterLocation]
+    class Shelter:
+        def __init__(self):
+            self.shelterId = '0'
+            self.shelterLocation = 'Orlando-Fl'
 
-    def validation(self, _type):
-        if _type == "dog" or _type == "cat":
-            print('everything looks fine')
-        else:
-            while _type not in ["cat", "dog"]:
-                print('======= Oops, type dog or cat =======')
-                _type = input()
-                if _type == "dog" or _type == "cat":
-                    self.printPetProfile()
-                    return True
+    class Pet(Shelter):
+        def __init__(self, _name, _age, _type):
+            super().__init__()
+            self.name = _name
+            self.age = _age
+            self.type = _type
+            self.validation(self.type)
+            self.petProfile = [_name, _age, _type, self.shelterLocation]
 
-    def printPetProfile(self):
-        print('from print profile')
+        def validation(self, _type):
+            if _type == "dog" or _type == "cat":
+                print('everything looks fine')
+            else:
+                while _type not in ["cat", "dog"]:
+                    print('======= Oops, type dog or cat =======')
+                    _type = input()
+                    if _type == "dog" or _type == "cat":
+                        self.printPetProfile()
+                        return True
 
-class Menu():
-    def __init__(self):
-        self.show()
+        def printPetProfile(self):
+            print('from print profile')
 
-    def show(self):
-        print('============ Welcome to the Shelter Dashboard ============' "\n"
-              '1) Show pets' "\n"
-              '2) Create a new pet' "\n")
-        self.exec()
+    class Menu():
+        def __init__(self):
+            self.show()
 
-    def exec(self):
-        menu_option = input()
-        if menu_option == "1":
-            print('show pets')
-        elif menu_option == "2":
-            # name = input("Type the name of the pet" "\n")
-            # age = input("Type the age" "\n")
-            # pet_type = input("Type the type" "\n")
-            # newPet = Pet(name, age, pet_type)
-            newPet = Pet('rhuan', '10', 'fsafas')
-        else:
-            while menuoption not in ["1", "2"]:
-                print('this is not a valid option')
-                menuoption = input()
-        return True
+        def show(self):
+            print('============ Welcome to the Shelter Dashboard ============' "\n"
+                  '1) Show pets' "\n"
+                  '2) Create a new pet' "\n")
+            self.exec()
 
-# ============ Initial Function ============
-def main():
-    menu = Menu()
+        def exec(self):
+            menu_option = input()
+            if menu_option == "1":
+                print('show pets')
+            elif menu_option == "2":
+                # name = input("Type the name of the pet" "\n")
+                # age = input("Type the age" "\n")
+                # pet_type = input("Type the type" "\n")
+                # newPet = Pet(name, age, pet_type)
+                newPet = Pet('rhuan', '10', 'fsafas')
+            else:
+                while menuoption not in ["1", "2"]:
+                    print('this is not a valid option')
+                    menuoption = input()
+            return True
 
-if __name__ == "__main__": main()# == auto exec
+    # ============ Initial Function ============
+    def main():
+        menu = Menu()
+
+    if __name__ == "__main__": main()# == auto exec
